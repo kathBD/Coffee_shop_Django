d0bdeacfeb2aae8a831c6e2c4020960558928ab1
kathBD <felicidadkg@gmail.com>
2024-08-02 12:46:55 -0500

    realizando vista formulario registro y url

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import ProductFormView
 
 urlpatterns = [
+    path('', ProductListView-as_view(), name='list_product')
     path('agregar/', ProductFormView.as_view(), name="add_product" ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductForm
 
@@ -7,3 +9,8 @@ from .forms import ProductForm
 class ProductFormView(generic.FormView):
     template_name = 'products/add_product.html'
     form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
