fb79062c9fd7591c70c96c6ab55c5c089cf3bcc9
kathBD <felicidadkg@gmail.com>
2024-08-01 20:23:40 -0500

    Add forms products

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Models):
+class Product(models.Model):
     name = models.TextField(max_length=200, verbose_name="nombre")
     description = models.TextField(max_length=300, verbose_name="descripción")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
