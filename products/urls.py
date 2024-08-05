bb248d7dec2a75b21032d58e561d294ac69c20ea
kathBD <felicidadkg@gmail.com>
2024-08-05 14:40:23 -0500

    template lsita de productos

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from .views import ProductFormView
+from .views import ProductListView
+
 
 urlpatterns = [
-    path('', ProductListView-as_view(), name='list_product')
-    path('agregar/', ProductFormView.as_view(), name="add_product" ),
+   path('', ProductListView.as_view(), name="list_product"),
+   path('agregar/', ProductFormView.as_view(), name="add_product" ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
+
+from products.models import Product
 from .forms import ProductForm
 
 
 # Create your views here.
 
+
 class ProductFormView(generic.FormView):
-    template_name = 'products/add_product.html'
+    template_name = "products/add_product.html"
     form_class = ProductForm
-    success_url = reverse_lazy('add_product')
-    
+    success_url = reverse_lazy("add_product")
+
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html' # Asegúrate de que el nombre sea correcto 
+    context_object_name = "products"
