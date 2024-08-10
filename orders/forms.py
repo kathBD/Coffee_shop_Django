2f94d52b2e0c4dad7fbf2428d763f1824395395b
kathBD <felicidadkg@gmail.com>
2024-08-09 20:07:57 -0500

    creando pedidos y footer

--- orders/forms.py
+++ orders/forms.py
@@ -6,6 +6,9 @@ class OrderProductForm(forms.ModelForm):
     class Meta:
         model = OrderProduct
         fields = ['product', 'quantity']  # Include 'quantity' if needed
+        widgets = {
+            'product': forms.Select(attrs={'class': 'border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'}),
+        }
 
 
 
